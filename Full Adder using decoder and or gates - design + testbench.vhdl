b7f99bff2a0fcdeed822160608ca8d1f48f366d0
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2024-03-10 17:51:52 +0200

    Update Full Adder using decoder and or gates - design + testbench.vhdl

--- Full Adder using decoder and or gates - design + testbench.vhdl
+++ Full Adder using decoder and or gates - design + testbench.vhdl
@@ -35,3 +35,40 @@ begin
 or_out<= (or_in(0) or or_in(1) or or_in(2) or or_in(3)) after or_delay;
 
 end behavioral;          
+
+entity Full_Adder is 
+port (A, B, C: in std_logic;
+sum, cout: out std_logic);
+end Full_Adder;
+
+architecture behavior of Full_Adder is 
+signal Decoder_Or_Connections: std_logic_vector (7 downto 0);
+
+component Decoder_8X3 is
+port (Decoder_in: in std_logic_vector (2 downto 0);
+Decoder_out: out std_logic_vector (7 downto 0));
+end component;
+
+component Or_4_inputs is 
+port(or_in: in std_logic_vector (3 downto 0);
+or_out: out std_logic);
+end component;
+
+begin
+
+Decoder_inst: Decoder_8X3 port map (Decoder_in(2)=>A, Decoder_in(1)=>B, Decoder_in(0)=>C, Decoder_out=>Decoder_Or_Connections);
+or_up_inst: or_4_inputs port map (
+or_in (3) => Decoder_Or_Connections (1),
+or_in (2) => Decoder_Or_Connections (2),
+or_in (1) => Decoder_Or_Connections (4),
+or_in (0) => Decoder_Or_Connections (7),
+or_out => Sum);
+
+or_down_inst: or_4_inputs port map (
+or_in (3) => Decoder_Or_Connections (3),
+or_in (2) => Decoder_Or_Connections (5),
+or_in (1) => Decoder_Or_Connections (6),
+or_in (0) => Decoder_Or_Connections (7),
+or_out => Cout);
+
+end behavior;
