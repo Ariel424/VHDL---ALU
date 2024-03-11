e1b387af9e58a42ebeb1fdece93cf70b6b6f2da2
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2024-03-11 09:36:54 +0200

    Update ALU.vhdl

--- ALU.vhdl
+++ ALU.vhdl
@@ -30,3 +30,37 @@ and_gate after Mux_Delay when "10",
 'X' after System_delay when others;
 
 end Behavioral; 
+
+entity ALU_TB is 
+  -- port();
+end entity; 
+
+architecture Behavioral of ALU_TB is 
+component ALU
+port (x, y, Cin: in std_logic;
+OPC: in std_logic_vector (1 downto 0);
+result, cout: out std_logic);
+end component;
+
+signal x, y, cin, result, cout: std_logic;
+signal OPC: std_logic_vector (1 downto 0);
+
+begin
+U1: ALU port map (x=>x, y=>y, cin=>cin, result=>result, cout=>cout, OPC=>OPC); 
+
+process
+begin
+wait for 10ns;
+
+for j in 0 to 2 loop 
+for i in 0 to 3 loop
+Test_case (std_logic_vector (to_unsigned (j, 2)));
+end loop;
+end loop;
+wait;
+end process;
+
+end Behavioral; 
+
+  
+    
